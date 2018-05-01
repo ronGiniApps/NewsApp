1981fda701668b1b73ffb7ad479b006169e8dc0f
Ron Haimovich <ronhaimovich@imac-2.local>
2018-05-01 15:08:57 +0300

    Initial Commit

---
+++ NewApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewApp
+//
+//  Created by Ron Haimovich on 01/05/2018.
+//  Copyright © 2018 Ron Haimovich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
